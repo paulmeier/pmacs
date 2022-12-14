5a68eb3a68cae12211262a33866671db03173494
Paul Meier <paulmartinmeier@gmail.com>
2022-12-14 14:54:25 -0600

    remove stuff

--- init.el
+++ init.el
@@ -49,17 +49,3 @@
          ("C-d" . ivy-reverse-i-search-kill))
   :config
   (ivy-mode 1))
-
-(setq user-emacs-directory "~/.config/pmacs/")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(ivy use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
